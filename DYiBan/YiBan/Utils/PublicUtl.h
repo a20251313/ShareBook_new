5538dbe98ba11de5fc11e35fd1ed1202ebbc8be7
a20251313 <a20251313@163.com>
2014-07-08 17:10:28 +0800

    address OK

---
+++ DYiBan/YiBan/Utils/PublicUtl.h
@@ -0,0 +1,13 @@
+//
+//  PublicUtl.h
+//  ShareBook
+//
+//  Created by Joshon on 14-7-8.
+//  Copyright (c) 2014年 Tomgg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PublicUtl : NSObject
+
+@end
---
+++ DYiBan/YiBan/Utils/PublicUtl.m
@@ -0,0 +1,13 @@
+//
+//  PublicUtl.m
+//  ShareBook
+//
+//  Created by Joshon on 14-7-8.
+//  Copyright (c) 2014年 Tomgg. All rights reserved.
+//
+
+#import "PublicUtl.h"
+
+@implementation PublicUtl
+
+@end
